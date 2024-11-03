ef9de6493ac49a6455149e35e871cc1a28f189ee
Valen <valentino.02@live.com.ar>
2024-11-03 20:29:39 -0300

    Conexion entre Tablas

--- Tp_3_Grupo_1.sql
+++ Tp_3_Grupo_1.sql
@@ -106,7 +106,7 @@ BEGIN
     CREATE TABLE ddbba.catalogo (
         id int IDENTITY (1,1) PRIMARY KEY, -- Llave primaria autoincremental
         category VARCHAR(100), -- Categoría del producto
-        nombre VARCHAR(100), -- Nombre del producto
+        nombre NVARCHAR(100) UNIQUE, -- Nombre del producto
         price DECIMAL(10, 2) CHECK (price > 0), -- Precio del producto, debe ser mayor a 0
         reference_price DECIMAL(10, 2), -- Precio de referencia
         reference_unit VARCHAR(2), -- Unidad de referencia
@@ -115,6 +115,25 @@ BEGIN
     );
 END;
 GO
+
+-- Verifica si la tabla 'Empleados' ya existe, si no, la crea.
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'ddbba.Empleados') AND type in (N'U'))
+BEGIN
+    CREATE TABLE ddbba.Empleados (
+		Legajo INT PRIMARY KEY, --Numero unico que representa a cada Empleado
+		Nombre VARCHAR(30), --Nombre del Empleado
+		Apellido VARCHAR(20), --Apellido del Empleado
+		DNI CHAR(8) CHECK (DNI LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'), --DNI del Empleado
+		Direccion VARCHAR(150), --Direccion del Empleado
+        EmailPersonal VARCHAR(100), --Email Personal del Empleado 
+        EmailEmpresa VARCHAR(100), --Email Empresarial del Empleado
+		CUIL VARCHAR (100), --CUIL del Empleado
+		Cargo VARCHAR(30) CHECK (Cargo IN ('Cajero', 'Supervisor', 'Gerente de sucursal')),--Cargo del Empleado
+		Sucursal VARCHAR(30) CHECK (Sucursal IN ('Ramos Mejia', 'Lomas del Mirador', 'San Justo')), --Sucursal a la cual corresponde el Empleado
+		Turno VARCHAR(30) CHECK (Turno IN ('TM', 'TT', 'Jornada completa')), --Turno en el que trabaja el Empleado
+		Activo BIT DEFAULT 1 --Campo para borrado logico
+    );
+END;
 
 
 -- Verifica si la tabla 'ventasRegistradas' ya existe, si no, la crea.
@@ -126,13 +145,13 @@ BEGIN
         Ciudad VARCHAR(50), -- Ciudad de la venta
         TipoCliente VARCHAR(30), -- Tipo de cliente
         Genero VARCHAR(10) CHECK (Genero IN ('Male', 'Female')), -- Género del cliente
-        Producto NVARCHAR(100), -- Nombre del producto
+        Producto NVARCHAR(100) CONSTRAINT FK_Producto_Catalogo FOREIGN KEY (Producto) REFERENCES ddbba.catalogo (nombre), -- Nombre del producto
         PrecioUnitario DECIMAL(10, 2), -- Precio unitario del producto
         Cantidad INT, -- Cantidad de productos
         Fecha DATE, -- Fecha de la venta
         Hora TIME, -- Hora de la venta
         MedioPago VARCHAR(20) CHECK (MedioPago IN ('Ewallet', 'Cash', 'Credit Card')), -- Medio de pago utilizado
-        Empleado INT, -- Identificador del empleado
+        Empleado INT CONSTRAINT FK_Empleado_IDEmpleado FOREIGN KEY (Empleado) REFERENCES ddbba.Empleados (Legajo), -- Identificador del empleado
         IdentificadorPago VARCHAR(25),
 			CHECK (
 			(MedioPago = 'Ewallet' AND IdentificadorPago LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]') OR
@@ -160,24 +179,7 @@ END;
 
 GO
 
--- Verifica si la tabla 'Empleados' ya existe, si no, la crea.
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'ddbba.Empleados') AND type in (N'U'))
-BEGIN
-    CREATE TABLE ddbba.Empleados (
-		Legajo INT PRIMARY KEY, --Numero unico que representa a cada Empleado
-		Nombre VARCHAR(30), --Nombre del Empleado
-		Apellido VARCHAR(20), --Apellido del Empleado
-		DNI CHAR(8) CHECK (DNI LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'), --DNI del Empleado
-		Direccion VARCHAR(150), --Direccion del Empleado
-        EmailPersonal VARCHAR(100), --Email Personal del Empleado 
-        EmailEmpresa VARCHAR(100), --Email Empresarial del Empleado
-		CUIL VARCHAR (100), --CUIL del Empleado
-		Cargo VARCHAR(30) CHECK (Cargo IN ('Cajero', 'Supervisor', 'Gerente de sucursal')),--Cargo del Empleado
-		Sucursal VARCHAR(30) CHECK (Sucursal IN ('Ramos Mejia', 'Lomas del Mirador', 'San Justo')), --Sucursal a la cual corresponde el Empleado
-		Turno VARCHAR(30) CHECK (Turno IN ('TM', 'TT', 'Jornada completa')), --Turno en el que trabaja el Empleado
-		Activo BIT DEFAULT 1 --Campo para borrado logico
-    );
-END;
+
 
 GO
 -- Verifica si la tabla 'ClasificacionProductos' ya existe, si no, la crea.
@@ -280,6 +282,32 @@ END
 
 GO
 
+--Sucursal
+
+IF EXISTS (SELECT * FROM sys.objects 
+           WHERE object_id = OBJECT_ID(N'ddbba.InsertarSucursal') 
+           AND type = N'P')
+BEGIN
+    PRINT 'El SP InsertarSucursal ya existe en el esquema ddbba.'
+END
+ELSE
+BEGIN
+    EXEC('CREATE PROCEDURE ddbba.InsertarSucursal
+		@Ciudad VARCHAR(50),
+        @ReemplazarPor VARCHAR(50), 
+        @Direccion VARCHAR(200), 
+        @Horario VARCHAR(50),
+        @Telefono VARCHAR(20) 
+AS
+BEGIN
+    INSERT INTO ddbba.Sucursal(Ciudad,ReemplazarPor,Direccion,Horario,Telefono)
+    VALUES (@Ciudad,@ReemplazarPor,@Direccion,@Horario,@Telefono);
+END')
+END
+
+GO
+
+
 --ventasRegistradas
 
 IF EXISTS (SELECT * FROM sys.objects 
@@ -306,6 +334,7 @@ BEGIN
         @IdentificadorPago VARCHAR(100) 
 AS
 BEGIN
+
     INSERT INTO ddbba.ventasRegistradas(IDFactura,TipoFactura,Ciudad,TipoCliente,Genero,Producto,PrecioUnitario,Cantidad,Fecha,Hora,MedioPago,Empleado,IdentificadorPago)
     VALUES (@IDFactura,@TipoFactura,@Ciudad,@TipoCliente,@Genero,@Producto,@PrecioUnitario,@Cantidad,@Fecha,@Hora,@MedioPago,@Empleado,@IdentificadorPago);
 END;')
@@ -315,30 +344,7 @@ END
 
 GO
 
---Sucursal
 
-IF EXISTS (SELECT * FROM sys.objects 
-           WHERE object_id = OBJECT_ID(N'ddbba.InsertarSucursal') 
-           AND type = N'P')
-BEGIN
-    PRINT 'El SP InsertarSucursal ya existe en el esquema ddbba.'
-END
-ELSE
-BEGIN
-    EXEC('CREATE PROCEDURE ddbba.InsertarSucursal
-		@Ciudad VARCHAR(50),
-        @ReemplazarPor VARCHAR(50), 
-        @Direccion VARCHAR(200), 
-        @Horario VARCHAR(50),
-        @Telefono VARCHAR(20) 
-AS
-BEGIN
-    INSERT INTO ddbba.Sucursal(Ciudad,ReemplazarPor,Direccion,Horario,Telefono)
-    VALUES (@Ciudad,@ReemplazarPor,@Direccion,@Horario,@Telefono);
-END')
-END
-
-GO
 
 --Empleados
 
