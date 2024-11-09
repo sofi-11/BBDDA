764974575a804ccbeb65b4b3a17ba3321dfd1ce9
sofi <sgay056@alumno.unlam.edu.ar>
2024-11-09 16:28:24 -0300

    hola

--- AuroraSA_solucion/AuroraSA_solucion/reporte.sql
+++ AuroraSA_solucion/AuroraSA_solucion/reporte.sql
@@ -6,6 +6,7 @@ RECONFIGURE;
 EXEC sp_configure 'xp_cmdshell', 1;
 RECONFIGURE;
 
+
 --Mensual: ingresando un mes y año determinado mostrar el total facturado por días de
 --la semana, incluyendo sábado y domingo.
 
@@ -45,3 +46,57 @@ BEGIN
     EXEC sp_executesql @sql, N'@mes INT, @anio INT', @mes, @anio;
 END;
 
+
+
+
+
+--Trimestral: mostrar el total facturado por turnos de trabajo por mes.
+
+EXEC FacturacionTrimestralPorTurnosPorMes @turno = 'Mañana', @trimestre = 1, @anio = 2019;
+
+CREATE OR ALTER PROCEDURE FacturacionTrimestralPorTurnosPorMes
+    @turno VARCHAR(50),  -- Turno del empleado para el reporte
+    @trimestre INT,      -- Trimestre para el reporte
+    @anio INT            -- Año para el reporte
+AS
+BEGIN
+    -- Declaramos una variable para construir la consulta dinámica en formato XML
+    DECLARE @sql NVARCHAR(MAX);
+
+    -- Construimos la consulta dinámica en la variable @sql
+    SET @sql = '
+        SELECT 
+            SUM(PrecioUnitario * Cantidad) AS TotalFacturacion,
+            CASE 
+                WHEN DATEPART(HOUR, hora) BETWEEN 8 AND 13 THEN ''Mañana''
+                WHEN DATEPART(HOUR, hora) BETWEEN 14 AND 23 THEN ''Tarde''
+            END AS Turno,
+            MONTH(Fecha) AS Mes
+        FROM ddbba.ventasRegistradas
+        WHERE 
+            YEAR(Fecha) = @anio 
+            AND ( 
+                (@trimestre = 1 AND MONTH(Fecha) IN (1, 2, 3)) OR
+                (@trimestre = 2 AND MONTH(Fecha) IN (4, 5, 6)) OR
+                (@trimestre = 3 AND MONTH(Fecha) IN (7, 8, 9)) OR
+                (@trimestre = 4 AND MONTH(Fecha) IN (10, 11, 12))
+            )
+            AND CASE 
+                    WHEN DATEPART(HOUR, hora) BETWEEN 8 AND 13 THEN ''Mañana''
+                    WHEN DATEPART(HOUR, hora) BETWEEN 14 AND 23 THEN ''Tarde''
+                END = @turno
+        GROUP BY 
+            CASE 
+                WHEN DATEPART(HOUR, hora) BETWEEN 8 AND 13 THEN ''Mañana''
+                WHEN DATEPART(HOUR, hora) BETWEEN 14 AND 23 THEN ''Tarde''
+            END,
+            MONTH(Fecha);
+    ';
+
+    -- Ejecutamos la consulta dinámica
+    EXEC sp_executesql @sql,
+        N'@turno VARCHAR(50), @trimestre INT, @anio INT',
+        @turno = @turno, 
+        @trimestre = @trimestre, 
+        @anio = @anio;
+END;
